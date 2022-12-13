9fdfdb9d8374c6aa4438fa74e28ca07331a0e70c
Vickrant Bawankule <95314283+Vickrant07@users.noreply.github.com>
2022-12-13 13:54:27 +0000

    Update settings.py

--- studyRoom/settings.py
+++ studyRoom/settings.py
@@ -140,6 +140,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
